5a0f1bfdb2538140c4dd1ace8190e84866f4a1cc
Mayank-Radadiya <mayankradadiya2@gmail.com>
2024-09-25 18:20:43 +0530

    first commit

--- 06_Currency_Converter/src/App.jsx
+++ 06_Currency_Converter/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   const currencyInfo = useCurrencyInfo(from);
 
-  const Options = Object.keys(currencyInfo);
+  const options = Object.keys(currencyInfo);
 
   const swap = () => {
     setFrom(to);
@@ -26,7 +26,7 @@ function App() {
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
-        backgroundImage: `url('https://images.unsplash.com/photo-1622630732303-8e94514a1746?q=80&w=2953&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
+        backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
       }}
     >
       <div className="w-full">
@@ -34,39 +34,42 @@ function App() {
           <form
             onSubmit={(e) => {
               e.preventDefault();
-              convert()
+              convert();
             }}
           >
             <div className="w-full mb-1">
-              <InputBox label="From" 
-              amount={amount }
-               CurrencyOptions={Options}
-               onCurrencyChange={  () => setAmount(amount)}
-               selectCurrency={from}
-               onAmountChange={ (amount) => {
-                setAmount(amount)
-               } }
-               />
-            </div> 
+              <InputBox
+                label="From"
+                amount={amount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setAmount(amount)}
+                selectCurrency={from}
+                onAmountChange={(amount) => setAmount(amount)}
+              />
+            </div>
             <div className="relative w-full h-0.5">
               <button
                 type="button"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
                 onClick={swap}
-                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5">
+              >
                 swap
               </button>
             </div>
             <div className="w-full mt-1 mb-4">
-              <InputBox label="To" 
-              amount={convertedAmount }
-              CurrencyOptions={Options}
-              onCurrencyChange={  () => setTo(amount)}
-              selectCurrency={from}
+              <InputBox
+                label="To"
+                amount={convertedAmount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setTo(currency)}
+                selectCurrency={from}
+                amountDisable
               />
             </div>
             <button
               type="submit"
-              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg" >
+              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+            >
               Convert {from.toUpperCase()} to {to.toUpperCase()}
             </button>
           </form>
--- 06_Currency_Converter/src/Hooks/useCurrencyInfo.js
+++ 06_Currency_Converter/src/Hooks/useCurrencyInfo.js
@@ -1,19 +1,18 @@
-import { useEffect , useState } from "react";
+import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
-     
-    const [Data , setData] = useState({})
-    
-    useEffect ( ( ) => {
-         
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json`)
-        .then( (res) => res.json()  )
-        .then( (res) => setData(res[currency]))
-        console.log(Data);
+  const [Data, setData] = useState({});
 
-     } ,[currency] )
-     console.log(Data);
-     return Data;
- }
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/usd.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+    console.log(Data);
+  }, [currency]);
+  console.log(Data);
+  return Data;
+}
 
- export default useCurrencyInfo;
+export default useCurrencyInfo;
